3fc5db53cddf827030a58e1fae9add9aaebec556
valentiniriar <vamair1708@gmail.com>
2025-06-19 18:55:13 -0300

    Img Hero

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
   return (
     <section id="about" className="section-padding">
       <div className="container grid-2-col">
         <div className="about-text">
-          <h2 className="section-title">Somos <span className="text-primary">Nordev</span>.</h2>
+          <h2 className="section-title">
+            Somos <span className="text-primary">Nordev</span>.
+          </h2>
           <p className="section-subtitle">
-            Somos un equipo de desarrolladores innovadores dedicados a construir soluciones digitales excepcionales. En Nordev, nos especializamos en transformar desafíos complejos en procesos elegantes, eficientes y automatizados que impulsan el crecimiento y el éxito de nuestros clientes.
+            Somos un equipo de desarrolladores innovadores dedicados a construir
+            soluciones digitales excepcionales. En Nordev, nos especializamos en
+            transformar desafíos complejos en procesos elegantes, eficientes y
+            automatizados que impulsan el crecimiento y el éxito de nuestros
+            clientes.
           </p>
         </div>
         <div className="placeholder-image">
-          <p>Imagen Opcional del Equipo o Espacio de Trabajo</p>
+          <p></p>
         </div>
       </div>
     </section>
